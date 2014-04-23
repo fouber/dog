6e42f518181c279328c6418d0164975e4706763a
fouber <fouber.NET@gmail.com>
2014-04-23 14:55:41 +0800

    update urls

--- src/fis-conf.js
+++ src/fis-conf.js
@@ -1,14 +1,19 @@
 fis.config.set('roadmap.path', [
     {
         reg : '**.mp3',
+        url : '/dog$&',
         useHash: true
     },
     {
         reg : 'map.json',
         release : false
-    }
+    },
     {
         reg : '**.sh',
         release : false
+    },
+    {
+        reg : '**',
+        url : '/dog$&'
     }
 ]);
