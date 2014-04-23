f7d4209565f1b7c5f51318fff584fcf70e29c1aa
fouber <fouber.NET@gmail.com>
2014-04-23 14:53:18 +0800

    first commit

---
+++ src/build.sh
@@ -0,0 +1,1 @@
+fis release -comd `pwd`/../
---
+++ src/fis-conf.js
@@ -0,0 +1,14 @@
+fis.config.set('roadmap.path', [
+    {
+        reg : '**.mp3',
+        useHash: true
+    },
+    {
+        reg : 'map.json',
+        release : false
+    }
+    {
+        reg : '**.sh',
+        release : false
+    }
+]);
